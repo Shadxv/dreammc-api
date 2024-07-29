40556d16d7a741f002ad73ce13dc5e9695a0a79b
Shadow6328 <kamil_0805@wp.pl>
2024-07-29 20:05:09 +0200

    Deployment init

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    `maven-publish`
+}
+
 dependencies {
     compileOnly(project(":shared"))
     compileOnly(project(":api"))
@@ -5,5 +9,35 @@ dependencies {
 }
 
 tasks.jar {
-    archiveBaseName.set("paper")
+    archiveFileName.set("api-paper.jar")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("SProjectAPI-Paper")
+                description.set("Paper version of API for SProject server")
+                url.set("https://nexus.senceye.com/")
+                scm {
+                    url.set("https://github.com/Secret-Project-Server/sproject-api")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Nexus"
+            url = uri(
+                "https://nexus.senceye.com/repository/maven-" + (project.findProperty("repo") as String? ?: "") + "/"
+            )
+            credentials {
+                username = "admin"
+                password = project.findProperty("nexusPassword") as String? ?: ""
+            }
+        }
+    }
 }
--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -1,3 +1,13 @@
+plugins {
+    `maven-publish`
+}
+
+dependencies {
+    compileOnly(project(":shared"))
+    compileOnly(project(":api"))
+    compileOnly("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
+}
+
 dependencies {
     compileOnly(project(":shared"))
     compileOnly(project(":api"))
@@ -5,5 +15,35 @@ dependencies {
 }
 
 tasks.jar {
-    archiveBaseName.set("velocity")
+    archiveFileName.set("api-velocity.jar")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("SProjectAPI-Velocity")
+                description.set("Velocity version of API for SProject server")
+                url.set("https://nexus.senceye.com/")
+                scm {
+                    url.set("https://github.com/Secret-Project-Server/sproject-api")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Nexus"
+            url = uri(
+                "https://nexus.senceye.com/repository/maven-" + (project.findProperty("repo") as String? ?: "") + "/"
+            )
+            credentials {
+                username = "admin"
+                password = project.findProperty("nexusPassword") as String? ?: ""
+            }
+        }
+    }
 }
