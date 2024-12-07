a12c8053d0f6e5d106ea916ca80d7e3df503a492
Shadow6328 <kamil_0805@wp.pl>
2024-12-07 02:27:51 +0100

    More click handlers

--- paper/src/main/java/pl/dreammc/dreammcapi/paper/listener/InteractInventoryListener.java
+++ paper/src/main/java/pl/dreammc/dreammcapi/paper/listener/InteractInventoryListener.java
@@ -33,6 +33,7 @@ public class InteractInventoryListener implements Listener {
         if(event.getClickedInventory() == null) return;
         if(event.getClickedInventory().getType() == InventoryType.PLAYER) {
             if(menu instanceof UnlockedMenu unlockedMenu && event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY) {
+                event.setCancelled(true);
                 Set<Integer> slots = this.getSlotToMoveItems(unlockedMenu, event.getCurrentItem());
                 if(slots == null) return;
                 int amountLeft = event.getCurrentItem().getAmount();
@@ -41,13 +42,29 @@ public class InteractInventoryListener implements Listener {
                     int slot = iterator.next();
                     @Nullable ItemStack inSlot = unlockedMenu.getHandledItems().get(slot);
                     if(inSlot == null) {
-                        unlockedMenu.handleChange(new ChangeAction(slot, event.getCurrentItem().asQuantity(amountLeft), ChangeType.SET_ITEM));
-                        break;
+                        ItemStack toAdd = event.getCurrentItem().asQuantity(amountLeft);
+                        unlockedMenu.handleChange(new ChangeAction(slot, toAdd, ChangeType.SET_ITEM));
+                        unlockedMenu.getInventory().setItem(slot, toAdd);
+                        unlockedMenu.addItemToContnet(slot, toAdd);
+                        amountLeft = 0;
+                    } else {
+                        int amoutDifference = inSlot.getMaxStackSize() - inSlot.getAmount();
+                        if (amoutDifference >= amountLeft) {
+                            ItemStack toAdd = event.getCurrentItem().asQuantity(event.getCurrentItem().getAmount() + amountLeft);
+                            unlockedMenu.handleChange(new ChangeAction(slot, event.getCurrentItem().asQuantity(event.getCurrentItem().getAmount() + amountLeft), ChangeType.SET_ITEM));
+                            unlockedMenu.getInventory().setItem(slot, toAdd);
+                            unlockedMenu.addItemToContnet(slot, toAdd);
+                        } else {
+                            ItemStack toAdd = event.getCurrentItem().asQuantity(event.getCurrentItem().getMaxStackSize());
+                            unlockedMenu.handleChange(new ChangeAction(slot, toAdd, ChangeType.SET_ITEM));
+                            unlockedMenu.getInventory().setItem(slot, toAdd);
+                            unlockedMenu.addItemToContnet(slot, toAdd);
+                        }
+                        amountLeft -= amoutDifference;
                     }
-                    int amoutDifference = inSlot.getMaxStackSize() - inSlot.getAmount();
-                    amountLeft -= amoutDifference;
-                    unlockedMenu.handleChange(new ChangeAction(slot, event.getCurrentItem().asQuantity(event.getCurrentItem().getMaxStackSize()), ChangeType.SET_ITEM));
                 }
+                if(amountLeft > 0) event.setCurrentItem(event.getCurrentItem().asQuantity(amountLeft));
+                else event.setCurrentItem(null);
             } else {
                 if(event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY) {
                     event.setCancelled(true);
@@ -64,11 +81,13 @@ public class InteractInventoryListener implements Listener {
                     ItemStack toReturn = cursor.clone();
                     if(inSlot != null) toReturn.add(inSlot.getAmount());
                     unlockedMenu.handleChange(new ChangeAction(event.getSlot(), toReturn, ChangeType.SET_ITEM));
+                    unlockedMenu.addItemToContnet(event.getSlot(), toReturn);
                 }
                 case PLACE_SOME -> {
                     ItemStack toReturn = event.getCursor().clone();
                     toReturn.setAmount(toReturn.getMaxStackSize());
                     unlockedMenu.handleChange(new ChangeAction(event.getSlot(), toReturn, ChangeType.SET_ITEM));
+                    unlockedMenu.addItemToContnet(event.getSlot(), toReturn);
                 }
                 case PLACE_ONE -> {
                     @Nullable ItemStack inSlot = event.getCurrentItem();
@@ -76,22 +95,50 @@ public class InteractInventoryListener implements Listener {
                     ItemStack toReturn = cursor.asOne();
                     if(inSlot != null) toReturn.add(inSlot.getAmount());
                     unlockedMenu.handleChange(new ChangeAction(event.getSlot(), toReturn, ChangeType.SET_ITEM));
+                    unlockedMenu.addItemToContnet(event.getSlot(), toReturn);
                 }
                 case PICKUP_ALL, DROP_ALL_SLOT, MOVE_TO_OTHER_INVENTORY -> {
                     unlockedMenu.handleChange(new ChangeAction(event.getSlot(), event.getCurrentItem(), ChangeType.REMOVE_ITEM));
+                    unlockedMenu.removeItemFromContent(event.getSlot());
                 }
                 case PICKUP_HALF -> {
                     ItemStack inSlot = event.getCurrentItem();
-                    if(inSlot.getAmount() == 1) unlockedMenu.handleChange(new ChangeAction(event.getSlot(), inSlot, ChangeType.REMOVE_ITEM));
-                    else unlockedMenu.handleChange(new ChangeAction(event.getSlot(), inSlot.asQuantity(inSlot.getAmount()/2), ChangeType.SET_ITEM));
+                    if(inSlot.getAmount() == 1) {
+                        unlockedMenu.handleChange(new ChangeAction(event.getSlot(), inSlot, ChangeType.REMOVE_ITEM));
+                        unlockedMenu.removeItemFromContent(event.getSlot());
+                    } else {
+                        ItemStack toReturn = inSlot.asQuantity(inSlot.getAmount()/2);
+                        unlockedMenu.handleChange(new ChangeAction(event.getSlot(), toReturn, ChangeType.SET_ITEM));
+                        unlockedMenu.addItemToContnet(event.getSlot(), toReturn);
+                    }
                 }
                 case PICKUP_ONE, DROP_ONE_SLOT -> {
                     ItemStack inSlot = event.getCurrentItem();
-                    if(inSlot.getAmount() == 1) if(inSlot.getAmount() == 1) unlockedMenu.handleChange(new ChangeAction(event.getSlot(), inSlot, ChangeType.REMOVE_ITEM));
-                    else unlockedMenu.handleChange(new ChangeAction(event.getSlot(), inSlot.asQuantity(inSlot.getAmount()-1), ChangeType.SET_ITEM));
+                    if(inSlot.getAmount() == 1) {
+                        unlockedMenu.handleChange(new ChangeAction(event.getSlot(), inSlot, ChangeType.REMOVE_ITEM));
+                        unlockedMenu.removeItemFromContent(event.getSlot());
+                    }
+                    else {
+                        ItemStack toReturn = inSlot.asQuantity(inSlot.getAmount()-1);
+                        unlockedMenu.handleChange(new ChangeAction(event.getSlot(), inSlot.asQuantity(inSlot.getAmount()-1), ChangeType.SET_ITEM));
+                        unlockedMenu.addItemToContnet(event.getSlot(), toReturn);
+                    }
                 }
                 case SWAP_WITH_CURSOR -> {
-                    unlockedMenu.handleChange(new ChangeAction(event.getSlot(), event.getCursor().clone(), ChangeType.SET_ITEM));
+                    ItemStack toReturn = event.getCursor().clone();
+                    unlockedMenu.handleChange(new ChangeAction(event.getSlot(), toReturn, ChangeType.SET_ITEM));
+                    unlockedMenu.addItemToContnet(event.getSlot(), toReturn);
+                }
+                case HOTBAR_SWAP -> {
+                    int hotbarSlot = event.getHotbarButton();
+                    ItemStack hotbarItemStack = player.getOpenInventory().getBottomInventory().getItem(hotbarSlot);
+                    if(hotbarItemStack != null && hotbarItemStack.getType() != Material.AIR) {
+                        unlockedMenu.handleChange(new ChangeAction(event.getSlot(), hotbarItemStack, ChangeType.SET_ITEM));
+                        unlockedMenu.addItemToContnet(event.getSlot(), hotbarItemStack);
+                    } else {
+                        unlockedMenu.handleChange(new ChangeAction(event.getSlot(), event.getCurrentItem(), ChangeType.REMOVE_ITEM));
+                        unlockedMenu.removeItemFromContent(event.getSlot());
+                    }
                 }
             }
             return;
@@ -112,7 +159,7 @@ public class InteractInventoryListener implements Listener {
         for(int i : unlockedMenu.getUnlockedSlots()) {
             if(unlockedMenu.getHandledItems().containsKey(i)) {
                 ItemStack itemInCurrentSlot = unlockedMenu.getHandledItems().get(i);
-                if(itemInCurrentSlot.getType() == itemStack.getType() && itemInCurrentSlot.getAmount() < itemInCurrentSlot.getMaxStackSize()) {
+                if(itemInCurrentSlot.asOne().equals(itemStack.asOne()) && itemInCurrentSlot.getAmount() < itemInCurrentSlot.getMaxStackSize()) {
                     slotsWithFillableItems.add(i);
                 }
             } else {
